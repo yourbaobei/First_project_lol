c20ffa017a852ef032a87ea1bbcbee5d9e339c4c
yourbaobei <sonyasemspb@gmail.com>
2024-12-15 14:00:22 +0300

    create_line

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("хелоу ворд")
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win, bg="white", height=400, width=400)
+for i in range(50, 400, 50):
+    canvas.create_line((0, i), (400, i), fill="black")
+    canvas.create_line((i, 0), (i, 400), fill="black")
+canvas.pack()
+tk.mainloop()
